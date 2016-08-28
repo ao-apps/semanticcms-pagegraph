105d6833bd968956dcfcf6c5490f71933a279a18
AO Industries, Inc. <support@aoindustries.com>
2016-08-28 17:29:22 -0500

    Moving page graph to own package

---
+++ src/main/java/com/semanticcms/pagegraph/PageGraphContextListener.java
@@ -0,0 +1,46 @@
+/*
+ * semanticcms-pagegraph - SemanticCMS component to view a graph of the current page and related pages.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-pagegraph.
+ *
+ * semanticcms-pagegraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-pagegraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-pagegraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.pagegraph;
+
+import com.semanticcms.core.servlet.SemanticCMS;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("Registers the CSS and scripts in SemanticCMS.")
+public class PageGraphContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
+		semanticCMS.addCssLink("/semanticcms-pagegraph/style.css");
+		semanticCMS.addScript("d3js", "/webjars/d3js/3.5.17/d3.min.js");
+		semanticCMS.addScript("dagre-d3", "/webjars/dagre-d3/0.4.10/dagre-d3.min.js");
+		semanticCMS.addScript("semanticcms-pagegraph", "/semanticcms-pagegraph/scripts.js");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Do nothing
+	}
+}
