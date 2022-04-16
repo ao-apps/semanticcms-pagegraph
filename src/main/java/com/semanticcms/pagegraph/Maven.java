4f54a1eb6c316fc017442b06a8acf3a320563810
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:48 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/pagegraph/Maven.java
+++ src/main/java/com/semanticcms/pagegraph/Maven.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-pagegraph.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.pagegraph;
 
 import com.aoapps.lang.Projects;
