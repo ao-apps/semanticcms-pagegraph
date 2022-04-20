7d2bd74203740459ce347d806f3f3b5027bfae89
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:58:35 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/com/semanticcms/pagegraph/PageGraph.java
+++ src/main/java/com/semanticcms/pagegraph/PageGraph.java
@@ -36,40 +36,40 @@ import javax.servlet.annotation.WebListener;
 @WebListener("Registers the CSS and scripts in RegistryEE and HtmlRenderer.")
 public class PageGraph implements ServletContextListener {
 
-	public static final Group.Name RESOURCE_GROUP = new Group.Name("semanticcms-pagegraph");
+  public static final Group.Name RESOURCE_GROUP = new Group.Name("semanticcms-pagegraph");
 
-	// TODO: Change to Group.Name once we have group-level ordering
-	public static final Style SEMANTICCMS_PAGEGRAPH = new Style("/semanticcms-pagegraph/semanticcms-pagegraph.css");
-	public static final Style SEMANTICCMS_PAGEGRAPH_PRINT = Style.builder()
-		.uri("/semanticcms-pagegraph/semanticcms-pagegraph-print.css")
-		.media("print")
-		.build();
+  // TODO: Change to Group.Name once we have group-level ordering
+  public static final Style SEMANTICCMS_PAGEGRAPH = new Style("/semanticcms-pagegraph/semanticcms-pagegraph.css");
+  public static final Style SEMANTICCMS_PAGEGRAPH_PRINT = Style.builder()
+    .uri("/semanticcms-pagegraph/semanticcms-pagegraph-print.css")
+    .media("print")
+    .build();
 
-	@Override
-	public void contextInitialized(ServletContextEvent event) {
-		ServletContext servletContext = event.getServletContext();
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    ServletContext servletContext = event.getServletContext();
 
-		// Add our CSS files
-		RegistryEE.Application.get(servletContext)
-			.activate(RESOURCE_GROUP) // TODO: Activate as-needed
-			.getGroup(RESOURCE_GROUP)
-			.styles
-			.add(
-				SEMANTICCMS_PAGEGRAPH,
-				SEMANTICCMS_PAGEGRAPH_PRINT
-			);
+    // Add our CSS files
+    RegistryEE.Application.get(servletContext)
+      .activate(RESOURCE_GROUP) // TODO: Activate as-needed
+      .getGroup(RESOURCE_GROUP)
+      .styles
+      .add(
+        SEMANTICCMS_PAGEGRAPH,
+        SEMANTICCMS_PAGEGRAPH_PRINT
+      );
 
-		HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
-		htmlRenderer.addScript("d3", "/webjars/d3/" + URIEncoder.encodeURIComponent(Maven.d3Version) + "/dist/d3.min.js");
-		htmlRenderer.addScript("lodash", "/webjars/lodash/" + URIEncoder.encodeURIComponent(Maven.lodashVersion) + "/lodash.min.js");
-		htmlRenderer.addScript("graphlib", "/webjars/graphlib/" + URIEncoder.encodeURIComponent(Maven.graphlibVersion) + "/dist/graphlib.min.js");
-		htmlRenderer.addScript("dagre", "/webjars/dagre/" + URIEncoder.encodeURIComponent(Maven.dagreVersion) + "/dist/dagre.min.js");
-		htmlRenderer.addScript("dagre-d3", "/webjars/dagre-d3/" + URIEncoder.encodeURIComponent(Maven.dagreD3Version) + "/dist/dagre-d3.min.js");
-		htmlRenderer.addScript("semanticcms-pagegraph", "/semanticcms-pagegraph/semanticcms-pagegraph.js");
-	}
+    HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
+    htmlRenderer.addScript("d3", "/webjars/d3/" + URIEncoder.encodeURIComponent(Maven.d3Version) + "/dist/d3.min.js");
+    htmlRenderer.addScript("lodash", "/webjars/lodash/" + URIEncoder.encodeURIComponent(Maven.lodashVersion) + "/lodash.min.js");
+    htmlRenderer.addScript("graphlib", "/webjars/graphlib/" + URIEncoder.encodeURIComponent(Maven.graphlibVersion) + "/dist/graphlib.min.js");
+    htmlRenderer.addScript("dagre", "/webjars/dagre/" + URIEncoder.encodeURIComponent(Maven.dagreVersion) + "/dist/dagre.min.js");
+    htmlRenderer.addScript("dagre-d3", "/webjars/dagre-d3/" + URIEncoder.encodeURIComponent(Maven.dagreD3Version) + "/dist/dagre-d3.min.js");
+    htmlRenderer.addScript("semanticcms-pagegraph", "/semanticcms-pagegraph/semanticcms-pagegraph.js");
+  }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		// Do nothing
-	}
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    // Do nothing
+  }
 }
--- src/main/resources/META-INF/resources/semanticcms-pagegraph/semanticcms-pagegraph.js
+++ src/main/resources/META-INF/resources/semanticcms-pagegraph/semanticcms-pagegraph.js
@@ -1,6 +1,6 @@
 /*
  * semanticcms-pagegraph - SemanticCMS component to view a graph of the current page and related pages.
- * Copyright (C) 2016  AO Industries, Inc.
+ * Copyright (C) 2016, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,53 +23,53 @@
 
 semanticcms_pagegraph = {
 
-	/*
-	 * Create the SVG label to pass in
-	 * Must create in SVG namespace
-	 * http://stackoverflow.com/questions/7547117/add-a-new-line-in-svg-bug-cannot-see-the-line
-	 * This mimics the same way string labels get added in Dagre-D3
-	 */
-	createDagNode : function(text, href, nodeCssClass) {
-		var svg_label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-		var tspan = document.createElementNS('http://www.w3.org/2000/svg','tspan');
-		tspan.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:space', 'preserve');
-		tspan.setAttribute('dy', '1em');
-		tspan.setAttribute('x', '1');
-		var link = document.createElementNS('http://www.w3.org/2000/svg', 'a');
-		link.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
-		//link.setAttribute('target', '_blank');
-		link.textContent = text;
-		tspan.appendChild(link);
-		svg_label.appendChild(tspan);
-		if(nodeCssClass === '') {
-			return {
-				label: svg_label,
-				labelType: 'svg'
-			};
-		} else {
-			return {
-				label: svg_label,
-				labelType: 'svg',
-				class: nodeCssClass
-			};
-		}
-	},
+  /*
+   * Create the SVG label to pass in
+   * Must create in SVG namespace
+   * http://stackoverflow.com/questions/7547117/add-a-new-line-in-svg-bug-cannot-see-the-line
+   * This mimics the same way string labels get added in Dagre-D3
+   */
+  createDagNode : function(text, href, nodeCssClass) {
+    var svg_label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    var tspan = document.createElementNS('http://www.w3.org/2000/svg','tspan');
+    tspan.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:space', 'preserve');
+    tspan.setAttribute('dy', '1em');
+    tspan.setAttribute('x', '1');
+    var link = document.createElementNS('http://www.w3.org/2000/svg', 'a');
+    link.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
+    //link.setAttribute('target', '_blank');
+    link.textContent = text;
+    tspan.appendChild(link);
+    svg_label.appendChild(tspan);
+    if (nodeCssClass === '') {
+      return {
+        label: svg_label,
+        labelType: 'svg'
+      };
+    } else {
+      return {
+        label: svg_label,
+        labelType: 'svg',
+        class: nodeCssClass
+      };
+    }
+  },
 
-	createDagEdge : function(text, href) {
-		var svg_edge_label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-		var edge_tspan = document.createElementNS('http://www.w3.org/2000/svg','tspan');
-		edge_tspan.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:space', 'preserve');
-		edge_tspan.setAttribute('dy', '1em');
-		edge_tspan.setAttribute('x', '1');
-		var edge_link = document.createElementNS('http://www.w3.org/2000/svg', 'a');
-		edge_link.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
-		//edge_link.setAttribute('target', '_blank');
-		edge_link.textContent = text;
-		edge_tspan.appendChild(edge_link);
-		svg_edge_label.appendChild(edge_tspan);
-		return {
-			labelType: "svg",
-			label: svg_edge_label
-		};
-	}
+  createDagEdge : function(text, href) {
+    var svg_edge_label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    var edge_tspan = document.createElementNS('http://www.w3.org/2000/svg','tspan');
+    edge_tspan.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:space', 'preserve');
+    edge_tspan.setAttribute('dy', '1em');
+    edge_tspan.setAttribute('x', '1');
+    var edge_link = document.createElementNS('http://www.w3.org/2000/svg', 'a');
+    edge_link.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
+    //edge_link.setAttribute('target', '_blank');
+    edge_link.textContent = text;
+    edge_tspan.appendChild(edge_link);
+    svg_edge_label.appendChild(edge_tspan);
+    return {
+      labelType: "svg",
+      label: svg_edge_label
+    };
+  }
 };
