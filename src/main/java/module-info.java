d991c9957bd051ba4c747b6d6f4a74909e117916
AO Industries, Inc <support@aoindustries.com>
2022-05-13 14:39:28 -0500

    Removed workaround for rewrite-maven-plugin-4.22.2 truncation issue
    We are not longer routinely performing rewrites during development and CI builds

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -33,4 +33,4 @@ module com.semanticcms.pagegraph {
   requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
   requires com.semanticcms.core.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-taglib</artifactId>
   requires taglibs.standard.spec; // <groupId>org.apache.taglibs</groupId><artifactId>taglibs-standard-spec</artifactId>
-} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
+}
